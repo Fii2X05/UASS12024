a13968e5e292ff87f19aebe3408d857dd0d542f4
RafiZFSIB1C <rafizf.fauzan25@gmail.com>
2024-12-18 13:59:35 +0700

    fix bug

--- UASSIB18.java
+++ UASSIB18.java
@@ -2,16 +2,16 @@
 //NIM : 244107060120
 //Absen : 18
 
-
 import java.util.Scanner;
 
 public class UASSIB18 {
-    static Scanner sc = new Scanner(System.in);
-    static String[] namaTim;
-    static int[][] skorTim; 
-    static int[] totalSkor; 
+    static Scanner sc18 = new Scanner(System.in);
+    static String[] namaTim18;
+    static int[][] skorTim18; 
+    static int[] totalSkor18; 
+
     public static void main(String[] args) {
-        int pilihan;
+        int pilihan18;
 
         do {
             System.out.println("\n=== MENU UTAMA ===");
@@ -20,18 +20,18 @@ public class UASSIB18 {
             System.out.println("3. Tentukan Juara");
             System.out.println("4. Keluar");
             System.out.print("Pilih menu (1-4): ");
-            pilihan = sc.nextInt();
-            sc.nextLine(); 
+            pilihan18 = sc18.nextInt();
+            sc18.nextLine();
 
-            switch (pilihan) {
+            switch (pilihan18) {
                 case 1:
-                    inputDataSkor();
+                    inputDataSkor18();
                     break;
                 case 2:
-                    tampilkanTabelSkor();
+                    tampilkanTabelSkor18();
                     break;
                 case 3:
-                    tentukanJuara();
+                    tentukanJuara18();
                     break;
                 case 4:
                     System.out.println("Terima kasih! Program selesai.");
@@ -39,47 +39,48 @@ public class UASSIB18 {
                 default:
                     System.out.println("Pilihan tidak valid. Coba lagi!");
             }
-        } while (pilihan != 4);
+        } while (pilihan18 != 4);
     }
 
-    static void inputDataSkor() {
-        int jumlahTim = (20 % 3) + 4; 
-        namaTim = new String[jumlahTim];
-        skorTim = new int[jumlahTim][2]; 
-        totalSkor = new int[jumlahTim]; 
-        System.out.println("\nInput Data Skor Tim:");
-        for (int i = 0; i < jumlahTim; i++) {
-            System.out.print("Masukkan nama tim ke-" + (i + 1) + ": ");
-            namaTim[i] = sc.nextLine();
+    static void inputDataSkor18() {
+        int jumlahTim18 = (20 % 3) + 4; 
+        namaTim18 = new String[jumlahTim18];
+        skorTim18 = new int[jumlahTim18][2];
+        totalSkor18 = new int[jumlahTim18];
 
-            for (int j = 0; j < 2; j++) {
-                int skor;
+        System.out.println("\nInput Data Skor Tim:");
+        for (int i18 = 0; i18 < jumlahTim18; i18++) {
+            System.out.print("Masukkan nama tim ke-" + (i18 + 1) + ": ");
+            namaTim18[i18] = sc18.nextLine();
+
+            for (int j18 = 0; j18 < 2; j18++) {
+                int skor18;
                 do {
-                    System.out.print("Masukkan skor " + namaTim[i] + " untuk Level " + (j + 1) + ": ");
-                    skor = sc.nextInt();
-                    if (skor < 0) {
+                    System.out.print("Masukkan skor " + namaTim18[i18] + " untuk Level " + (j18 + 1) + ": ");
+                    skor18 = sc18.nextInt();
+                    if (skor18 < 0) {
                         System.out.println("Skor tidak boleh negatif. Coba lagi!");
                     }
-                } while (skor < 0);
+                } while (skor18 < 0);
 
-                skorTim[i][j] = skor < 35 ? 0 : skor; 
+                skorTim18[i18][j18] = skor18 < 35 ? 0 : skor18; 
             }
-            sc.nextLine(); 
+            sc18.nextLine(); 
 
-            totalSkor[i] = skorTim[i][0] + skorTim[i][1];
+            totalSkor18[i18] = skorTim18[i18][0] + skorTim18[i18][1];
 
-            if (skorTim[i][0] > 50 && skorTim[i][1] > 50) {
-                totalSkor[i] += 18; 
+            if (skorTim18[i18][0] > 50 && skorTim18[i18][1] > 50) {
+                totalSkor18[i18] += 18;
             }
 
-            if (totalSkor[i] % 2 == 0) {
-                totalSkor[i] -= 15;
+            if (totalSkor18[i18] % 2 == 0) {
+                totalSkor18[i18] -= 15;
             }
         }
     }
 
-    static void tampilkanTabelSkor() {
-        if (namaTim == null) {
+    static void tampilkanTabelSkor18() {
+        if (namaTim18 == null) {
             System.out.println("Tidak ada data yang bisa ditampilkan.");
             return;
         }
@@ -87,37 +88,37 @@ public class UASSIB18 {
         System.out.println("\n=== Tabel Skor Tim ===");
         System.out.printf("%-15s%-10s%-10s%-10s\n", "Nama Tim", "Level 1", "Level 2", "Total Skor");
 
-        for (int i = 0; i < namaTim.length; i++) {
-            System.out.printf("%-15s%-10d%-10d%-10d\n", namaTim[i], skorTim[i][0], skorTim[i][1], totalSkor[i]);
+        for (int i18 = 0; i18 < namaTim18.length; i18++) {
+            System.out.printf("%-15s%-10d%-10d%-10d\n", namaTim18[i18], skorTim18[i18][0], skorTim18[i18][1], totalSkor18[i18]);
         }
     }
 
-    static void tentukanJuara() {
-        if (namaTim == null) {
+    static void tentukanJuara18() {
+        if (namaTim18 == null) {
             System.out.println("Tidak ada data untuk menentukan juara.");
             return;
         }
 
-        int maxSkor = totalSkor[0];
-        int juara = 0;
-        boolean seri = false;
+        int maxSkor18 = totalSkor18[0];
+        int juara18 = 0;
+        boolean seri18 = false;
 
-        for (int i = 1; i < totalSkor.length; i++) {
-            if (totalSkor[i] > maxSkor) {
-                maxSkor = totalSkor[i];
-                juara = i;
-                seri = false;
-            } else if (totalSkor[i] == maxSkor) {
-                if (skorTim[i][1] == skorTim[juara][1]) { 
-                    seri = true;
+        for (int i18 = 1; i18 < totalSkor18.length; i18++) {
+            if (totalSkor18[i18] > maxSkor18) {
+                maxSkor18 = totalSkor18[i18];
+                juara18 = i18;
+                seri18 = false;
+            } else if (totalSkor18[i18] == maxSkor18) {
+                if (skorTim18[i18][1] == skorTim18[juara18][1]) { 
+                    seri18 = true;
                 }
             }
         }
 
-        if (seri) {
+        if (seri18) {
             System.out.println("Turnamen berakhir seri. Tim terbaik adalah Rafi.");
         } else {
-            System.out.println("Selamat kepada Tim " + namaTim[juara] + " yang menjadi juara!");
+            System.out.println("Selamat kepada Tim " + namaTim18[juara18] + " yang menjadi juara!");
         }
     }
 }
